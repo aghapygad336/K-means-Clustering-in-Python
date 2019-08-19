afb9bb07b76d8cb2478b15d666613ce2459076b1
aghapygad336 <46167070+aghapygad336@users.noreply.github.com>
2019-08-19 16:51:03 +0200

    euclidean distances

--- sheet4ProblemA.py
+++ sheet4ProblemA.py
@@ -8,10 +8,17 @@ Created on Mon Aug 19 14:47:56 2019
 
 from sklearn.cluster import KMeans
 import numpy as np
+from sklearn.metrics.pairwise import euclidean_distances
+
+
 X = np.array([[2, 2], [1, 14], [10, 7],
               [1, 11], [3, 4], [11, 8],[4,3],[12,9]])
 kmeans = KMeans(n_clusters=3, random_state=0).fit(X)
 
 kmeans.predict([[2,2], [1,14],[4,2]])
+centroids=kmeans.cluster_centers_
 
-print (kmeans.cluster_centers_)
+
+print("Ecludian")
+test = euclidean_distances(X, X)
+print(test)
