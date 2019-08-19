3f606ef615688a72b792e34d4b224d47f9a28ab5
aghapygad336 <46167070+aghapygad336@users.noreply.github.com>
2019-08-19 18:39:02 +0200

    Manhattan Added

--- sheet4ProblemA.py
+++ sheet4ProblemA.py
@@ -11,6 +11,10 @@ import numpy as np
 from sklearn.metrics.pairwise import euclidean_distances
 
 
+def manhattan_distmtx(X, Y):
+    f = np.dot(X.sum(axis=1).reshape(-1, 1), Y.sum(axis=1).reshape(-1, 1).T)
+    return f / Y.sum(axis=1) - Y.sum(axis=1)
+
 X = np.array([[2, 2], [1, 14], [10, 7],
               [1, 11], [3, 4], [11, 8],[4,3],[12,9]])
 kmeans = KMeans(n_clusters=3, random_state=0).fit(X)
@@ -22,3 +26,9 @@ centroids=kmeans.cluster_centers_
 print("Ecludian")
 test = euclidean_distances(X, X)
 print(test)
+print("Moheto")
+
+
+
+manhattan_distmtx(X,X)
+
